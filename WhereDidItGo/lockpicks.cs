e1dbacb451c7a49480930c9367a58b91f67980b0
thewildvegetable <whichazelnj13@aol.com>
2020-08-06 09:23:56 -0400

    1 of many potential solutions

--- WhereDidItGo/lockpicks.cs
+++ WhereDidItGo/lockpicks.cs
@@ -20,12 +20,33 @@ namespace WhereDidItGo
         public void Get(int left, int right)
         {
             //fill in the code here to pick up the 2 lockpicks next to a thief if they are available. Hint: make sure the variables you are accessing can't be accessed by another thread while you are editing them
+            lock (this)
+            {
+                //if either the left or right lockpick is in use, wait for it to be available
+                while (this.lockpicks[left] || this.lockpicks[right])
+                {
+                    Monitor.Wait(this);
+                }
+
+                //grab the lockpicks
+                this.lockpicks[left] = true;
+                this.lockpicks[right] = true;
+            }
         }
 
         //put down lockpicks
         public void Put(int left, int right)
         {
             //fill in the code here to put down the 2 lockpicks that were used by a thief so others can use them.  Hint: make sure the variables you are accessing can't be accessed by another thread while you are editing them
+            lock (this)
+            {
+                //put the lockpicks down
+                this.lockpicks[left] = false;
+                this.lockpicks[right] = false;
+
+                //alert all thieves waiting for lockpicks
+                Monitor.PulseAll(this);
+            }
         }
     }
 }
--- WhereDidItGo/thief.cs
+++ WhereDidItGo/thief.cs
@@ -33,7 +33,42 @@ namespace WhereDidItGo
                 //fill in the code to actually pick the locks and perform the heist here. 
                 //When the Thief is picking the lock please add the following line
                 //Console.WriteLine("Thief " + (this._index + 1) + " is picking locks");
+                //check if the lockpicks near the thief are open
+                if (this._index < 4)
+                {
+                    this.lockpicks.Get(this._index, this._index + 1);
+                    heldLockpicks = 2;
+                }
+                //if thief 5 check if lockpick to the right is open
+                if (this._index == 4)
+                {
+                    this.lockpicks.Get(this._index, 0);
+                    heldLockpicks = 2;
+                }
 
+                //if you have 2 lockpicks, pick lock then place them down
+                if (heldLockpicks == 2)
+                {
+                    Console.WriteLine("Thief #" + (this._index + 1) + " is picking locks");
+                    this.pickedLocks++;
+                    Thread.Sleep(1000);
+
+
+                    //place both lockpicks down
+                    if (this._index == 4)
+                    {
+                        this.lockpicks.Put(this._index, 0);
+                    }
+                    else
+                    {
+                        this.lockpicks.Put(this._index, this._index + 1);
+                    }
+                    heldLockpicks = 0;
+
+
+                    //wait for a bit
+                    Thread.Sleep(1000);
+                }
             }
 
             //display that this Thief has picked the required amount of locks
