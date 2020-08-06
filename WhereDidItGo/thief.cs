50ba9c0379fb293af93b72398fbf73b5d5ac5a57
Alexander Brewer <abrewer@elmira.com>
2020-08-05 20:15:01 -0400

    Pushing challenge code.

--- WhereDidItGo/thief.cs
+++ WhereDidItGo/thief.cs
@@ -27,12 +27,42 @@ namespace WhereDidItGo
         public void Heist()
         {
             int heldLockpicks = 0;      //lockpicks in hand
+
+            // assigning which lockpicks will be taken by which thief based on ID
+            int leftHand = _index;
+            int rightHand;
+
+            if (_index == 4)
+            {
+                rightHand = 0;
+            }
+            else
+            {
+                rightHand = _index + 1;
+            }
+
+
             //start the heist
             while (this.pickedLocks < this.totalLocks)
             {
                 //fill in the code to actually pick the locks and perform the heist here. 
                 //When the Thief is picking the lock please add the following line
-                //Console.WriteLine("Thief " + (this._index + 1) + " is picking locks");
+
+                this.lockpicks.Get(leftHand, rightHand);
+                // thief now has lockpicks
+                Console.WriteLine("Thief " + (this._index + 1) + " is picking locks");
+                heldLockpicks = 2; // update number of held lockpicks
+                this.pickedLocks++;
+                Thread.Sleep(200); // sleep to allow other threads to handle work
+
+                this.lockpicks.Put(leftHand, rightHand);
+                heldLockpicks = 0;
+                Thread.Sleep(200);
+
+
+
+
+
 
             }
 
